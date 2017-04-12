b061bc53b8a12cd2d3699f14c32fca3e725e2b93
Chris Keller <cjameskeller@gmail.com>
2017-04-12 10:45:19 -0500

    factory done

--- client/scripts/factories/DataService.js
+++ client/scripts/factories/DataService.js
@@ -1,7 +1,41 @@
 myApp.factory('DataService', function(){
-  var dataObject = {};
+  //This will allow the data binding on Order page
+  var orderedPizza = {};
+  //This will store the pizzas to display to Checkout page
+  var orderedPizzas = {
+    orderedArray : []
+  };
+  //This adds the pizza to the orderedArray
+  var orderPizza = function(thisPizza){
+    orderedPizzas.orderedArray.push(thisPizza);
+  };
 
+  //No equivalent to orderedPizza above because we will ng-repeat on Checkout page
+  //     and then be pulling the specific pizza from the divs
+  //This will store the confirmed pizzas just because
+  var confirmedPizzas = {
+    confirmedArray : []
+  };
+  //This adds the pizzas to the confirmedArray
+  var confirmPizza = function(thisPizza){
+    confirmedPizzas.confirmedArray.push(thisPizza);
+  };
+
+  //Not confident this will work...
+  var removePizza = function(thisPizza){
+    var index = orderedPizzas.orderedArray.indexOf(thisPizza);
+   orderedPizzas.orderedArray.splice(index, 1);
+  };
+
+//RETURNS
   return {
-    dataObject : dataObject
+    orderedPizza : orderedPizza,
+    orderedPizzas : orderedPizzas,
+    orderPizza : orderPizza,
+
+    confirmedPizzas : confirmedPizzas,
+    confirmPizza : confirmPizza,
+
+    removePizza : removePizza
   };
 });
