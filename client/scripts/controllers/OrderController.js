41f801bab3c98afae648f47b2d94aa2c5594ece4
Kevin Dahlberg <nsu_tpt@hotmail.com>
2017-04-12 11:41:57 -0500

    worked on clearing the form on submit

--- client/scripts/controllers/OrderController.js
+++ client/scripts/controllers/OrderController.js
@@ -2,4 +2,10 @@ myApp.controller('OrderController', ['DataService', function(DataService){
   order = this;
   order.orderedPizza = DataService.orderedPizza;
   order.orderPizza = DataService.orderPizza;
+
+  order.clearForm = function(){
+    console.log("clear form clicked");
+    order.orderedPizza.size = '';
+    order.orderedPizza.toppings = '';
+  };
 }]);
